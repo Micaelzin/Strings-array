2c745267ce8813490b284b2cf9a7c4d2dbdf29db
micaelzin <wendellmicael773@gmail.com>
2023-04-28 21:15:06 -0300

    exercício

---
+++ index.js
@@ -0,0 +1,14 @@
+//  const frase = prompt("Escreva uma frase")
+//  const fraseMaiuscula =frase.toUpperCase() 
+//  console.log(fraseMaiuscula)
+//  const novaFrase = fraseMaiuscula.repleceA11("o","i")
+//  console.log(novaFrase);
+//  console.log(novaFrase.length);
+ 
+  const numeros = [1, 2, 3, 4, 5, 6]
+  console.log(numeros.lenght);
+  numeros.push(7)
+  console.log(numeros)
+  numeros.splice(3,2)
+  console.log(numeros)
+  console.log(numeros.lenght)
